ad0b6f1053dda13a484077d2e931973937863a8f
Brett Randall <javabrett@gmail.com>
2018-05-30 13:22:41 +1000

    Added HashLoginServiceTest.

---
+++ jetty-security/src/test/java/org/eclipse/jetty/security/HashLoginServiceTest.java
@@ -0,0 +1,102 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2018 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+
+package org.eclipse.jetty.security;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HashLoginServiceTest
+{
+    private final String testFileDir = "target" + File.separator + "property-user-store-test";
+    private final String testFile = "users.txt";
+    private Path testFilePath;
+
+    @Before
+    public void before() throws Exception
+    {
+        final Path testFileDirPath = Paths.get(testFileDir);
+        Files.createDirectories(testFileDirPath);
+        testFilePath = testFileDirPath.resolve(testFile).toAbsolutePath();
+
+        writeInitialUsers(testFilePath);
+    }
+
+    @After
+    public void after() throws Exception
+    {
+        Files.delete(testFilePath);
+    }
+
+    private void writeInitialUsers(Path testFilePath) throws Exception
+    {
+        try (Writer writer = new BufferedWriter(new FileWriter(testFilePath.toFile())))
+        {
+            writer.append("tom: tom, roleA\n");
+            writer.append("dick: dick, roleB\n");
+            writer.append("harry: harry, roleA, roleB\n");
+        }
+    }
+
+    @Test
+    public void testHashLoginServiceLoadFromSimplePath() throws Exception
+    {
+        testHashLoginService(testFilePath.normalize().toString());
+    }
+
+    @Test
+    public void testHashLoginServiceLoadFromUri() throws Exception
+    {
+        testHashLoginService(testFilePath.toUri().toString());
+    }
+
+    @Test
+    public void testHashLoginServiceLoadFromUrl() throws Exception
+    {
+        testHashLoginService(testFilePath.toUri().toURL().toString());
+    }
+
+    protected void testHashLoginService(String configPath) throws Exception
+    {
+        final HashLoginService hashLoginService = new HashLoginService("hashLoginService");
+        System.err.println("config path string: " + configPath);
+        hashLoginService.setConfig(configPath);
+        // don't setUserStore ...
+        // the behaviour tested is that of creation of default PropertyUserStore
+        try
+        {
+            hashLoginService.start();
+            assertNotNull("Can't find user dick", hashLoginService.login("dick", "dick", null));
+        }
+        finally
+        {
+            hashLoginService.stop();
+        }
+    }
+}
